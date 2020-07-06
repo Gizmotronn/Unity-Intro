8fbd68ff6df63bebc92356ab60b6c7c3e84c7173
Limo <liam@acordsoftware.tech>
2020-07-06 14:00:28 +0800

    Intro to Scripting | Unity Intro
    Clickup: CU-1ytqbu; https://share.clickup.com/t/h/1ytqbu/YFUXGFH8SZJ7YPR
    Co-Authored-By: Liam Arbuckle <allianceofdroidsla@gmail.com>

---
+++ Intro to Unity/Assets/Scripts/New Script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        logLevel();
+
+        //int level = 1; 
+        //Debug.Log(level);
+    }
+
+    void logLevel() 
+    {
+        int level = 2;
+        Debug.Log(level);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
