eec8b82e477666401d79d70f7b872efcb2645df9
Limo <liam@acordsoftware.tech>
2020-07-14 15:16:20 +0800

    #7
    CU-1yx336
    
    Co-Authored-By: Liam Arbuckle <allianceofdroidsla@gmail.com>

--- Intro to Unity/Assets/Scripts/Mover.cs
+++ Intro to Unity/Assets/Scripts/Mover.cs
@@ -9,13 +9,15 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Create vector3
-        //Vector3 newPos = new Vector3(5, 2, -2); // 5, 2, -2 = transform.position values for game object this script is attached to for the Vector3
-        //transform.position = newPos;
-        transform.position += new Vector3(0.1f, 01f, 0.1f); // every frame this object moves like this
-        transform.position += new Vector3(0.1f, 01f, 0.1f); * Time.deltaTime; // every second rather than every frame
-        transform.position += Vector3.one * Time.deltaTime; // .one = x, y, z
-        transform.position += new Vector3(0.1f, 01f, 0.1f) * Time.deltaTime * moveSpeed; // define moveSpeed in the inspector
-        transform.position += transform.forward * Time.deltaTime * moveSpeed; // Move capsule in its forward direction
+       transform.position += transform.forward * moveSpeed * Time.deltaTime; 
+
+       if(transform.position.z >= 3 || transform.position.z <= -3)
+       {
+           moveSpeed = -moveSpeed;
+       }
+      // else if(transform.position.z <= -3)
+       //{
+        //   moveSpeed = -moveSpeed
+       //}
     }
 }
