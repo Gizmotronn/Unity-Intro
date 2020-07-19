9e7c1244b8be68c4aef1c66400c4e97ba9ba55bd
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-07-19 15:37:18 +0800

    #8 Movement
    CU-20qy5g
    
    Co-Authored-By: Liam Arbuckle <allianceofdroidsla@gmail.com>

---
+++ 3D-Platformer/3D Platformer-Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed; // This will be set inside the Unity inspector for this object
+
+    private Rigidbody rig;
+
+    void Awake () 
+    {
+        // Get the rigidbody component
+        rig = GetComponent<Rigidbody>();
+    }
+
+    void Update () // Called every frame
+    {
+        Move(); // Every frame we're calling `move`
+    }
+
+    void Move () // To move the player
+    {
+        // Getting inputs; we'll then apply this to our velocity
+        float xInput = Input.GetAxis("Horizontal"); // Unity input method, horizontal axis /#/ x & y coords // If no button, it is set to 0
+        float zInput = Input.GetAxis("Vertical"); // Unity transform/position = x, y, z; x = x, z = y
+
+        // Velocity
+        Vector3 dir = new Vector3(xInput, 0, zInput) * moveSpeed;
+        rig.velocity = dir;
+        // Manually set y direction
+        dir.y = rig.velocity.y; // whatever the existing velocity is
+    }
+}
