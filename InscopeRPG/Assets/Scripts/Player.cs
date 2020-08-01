ed5a37ccd12c62d50287fec10e8d85c13b0f6ece
Limo <liam@acordsoftware.tech>
2020-08-01 11:13:56 +0800

    #1 #7 #2 -- Inscope Init - 1.0
    CU-20x4bn, CU-20vaz2
    
    https://www.youtube.com/watch?v=GjTTqz39kLY&list=PLX-uZVK_0K_6JEecbu3Y-nVnANJznCzix&index=3
    
    Set up game with www.inscope.me sprites, see prev commits + clickup
    
    Co-Authored-By: Liam Arbuckle <allianceofdroidsla@gmail.com>
    Co-Authored-By: Liam Arbuckle <liam@acordsoftware.tech>

---
+++ InscopeRPG/Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed; // For move speed / Adjustible in Unity inspector /#/ Set to "10" in inspector
+    private Vector2 direction; // player direction
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Vector2.up;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+        Move();
+    }
+
+    public void Move() {
+        transform.Translate(direction*speed*Time.deltaTime); // Time.deltaTime = time that has passed since the last update - same movement speed on all platforms/computers --> frames/updates per second
+    }
+
+    private void GetInput()
+    {
+        direction = Vector2.zero; // Prevent adding speed when more keys are pressed/when keys are held down for longer        
+
+        if (Input.GetKey(KeyCode.W)) // Get key - executed while holding down
+        {
+            direction += Vector2.up;
+        }
+
+        if (Input.GetKey(KeyCode.A)) 
+        {
+            direction += Vector2.left;
+        }
+
+        if (Input.GetKey(KeyCode.S)) 
+        {
+            direction += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.D)) 
+        {
+            direction += Vector2.right;
+        }
+    }
+}
