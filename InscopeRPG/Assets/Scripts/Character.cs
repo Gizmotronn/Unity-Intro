eaedf10c62f70f00b1ad623e147883fcbf92d3f1
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-08-03 17:58:51 +0800

    #1 Inscope Movement 1.2
    CU-22r6aw
    
    Co-Authored-By: Liam Arbuckle <allianceofdroidsla@gmail.com>

--- InscopeRPG/Assets/Scripts/Character.cs
+++ InscopeRPG/Assets/Scripts/Character.cs
@@ -2,73 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Character : MonoBehaviour 
-{
+/// <summary>
+/// This is an abstract class that all characters needs to inherit from
+/// </summary>
+public abstract class Character : MonoBehaviour {
+
+    /// <summary>
+    /// The Player's movement speed
+    /// </summary>
     [SerializeField]
-    private float speed; 
+    private float speed;
+
+    private Animator animator;
+
+    /// <summary>
+    /// The Player's direction
+    /// </summary>
     protected Vector2 direction;
 
-    private Animator animator;
-
-    void Start()
-    {
-        animator = GetComponent<Animator>; 
-    }
-
-    
-    protected virtual void Update()
-    {
-        Move();
-    }
-
-    public void Move() {
-        transform.Translate(direction*speed*Time.deltaTime); 
-        AnimateMovement();
-    }
-
-    public void AnimateMovement(Vector2 direction)
-    {
-        animator.SetFloat("x",direction.x);
-        animator.SetFloat("y",direction.y);
-    }
-}
-
-
-/*
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract/* Can't add this to objects, but can add some parts of this over to other objects class Character : MonoBehaviour 
-{
-    [SerializeField]
-    private float speed; // For move speed / Adjustible in Unity inspector /#/ Set to "10" in inspector
-    protected /* Can be accessed by any object with Character instead of MonoBehaviour  Vector2 direction; // player direction
-
-    // Animate variables
-    private Animator animator;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        animator = GetComponent<Animator>; 
-    }
-
-    // Update is called once per frame
-    protected virtual /* Can go into player.cs and override void Update()
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	/// <summary>
+    /// Update is marked as virtual, so that we can override it in the subclasses
+    /// </summary>
+	protected virtual void Update ()
     {
         Move();
+	}
+
+    /// <summary>
+    /// Moves the player
+    /// </summary>
+    public void Move()
+    {
+        //Makes sure that the player moves
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        //Animate's the Player's movement
+        AnimateMovement(direction);
     }
 
-    public void Move() {
-        transform.Translate(direction*speed*Time.deltaTime); // Time.deltaTime = time that has passed since the last update - same movement speed on all platforms/computers --> frames/updates per second
-        AnimateMovement();
-    }
-
+    /// <summary>
+    /// Makes the player animate in the correct direction
+    /// </summary>
+    /// <param name="direction"></param>
     public void AnimateMovement(Vector2 direction)
     {
-        animator.SetFloat("x",direction.x);
-        animator.SetFloat("y",direction.y);
+        //Sets the animation parameter so that he faces the correct direction
+        animator.SetFloat("x", direction.x);
+        animator.SetFloat("y", direction.y);
     }
 }
-*/
