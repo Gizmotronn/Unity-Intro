6f356c78c9ca6b048ca4c7981ccaf97b59a0f653
Limo <liam@acordsoftware.tech>
2020-08-04 21:01:33 +0800

    #1 - Miniscule Space Program - See desc
    unity when objects collide load scene
    
    Add wizard npc from inscope for something else, it will walk around (character.cs script)
    
    Add spaceship sprite, when character object collides with spaceship - load game
    
    unity when objects collide load scene
    
    Co-Authored-By: Liam Arbuckle <allianceofdroidsla@gmail.com>

---
+++ MinisculeSpaceProgram/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is the player script, it contains functionality that is specific to the Player
+/// </summary>
+public class Player : Character
+{
+
+	/// <summary>
+    /// We are overriding the characters update function, so that we can execute our own functions
+    /// </summary>
+	protected override void Update ()
+    {
+        //Executes the GetInput function
+        GetInput();
+
+        base.Update();
+	}
+
+    /// <summary>
+    /// Listen's to the players input
+    /// </summary>
+    private void GetInput()
+    {
+        direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W)) //Moves up
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.A)) //Moves left
+        {
+            direction += Vector2.left; //Moves down
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D)) //Moves right
+        {
+            direction += Vector2.right;
+        }
+    }
+}
