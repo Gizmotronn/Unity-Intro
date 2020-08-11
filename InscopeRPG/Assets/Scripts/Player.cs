41bc211694042df120e2249db9b9864af540aeaf
Limo <liam@acordsoftware.tech>
2020-08-11 11:48:09 +0800

    #1 Inscope RPG 2.0 - Part 2 - Stat & Player attachment
    Clickup:
    https://github.com/orgs/acord-robotics/teams/game-development/discussions/3
    
    More pixel art coming soon!
    
    Co-Authored-By: Liam Arbuckle <allianceofdroidsla@gmail.com>

--- InscopeRPG/Assets/Scripts/Player.cs
+++ InscopeRPG/Assets/Scripts/Player.cs
@@ -7,6 +7,101 @@ using UnityEngine;
 /// </summary>
 public class Player : Character
 {
+    /// <summary>
+    /// The player's health
+    /// </summary>
+    [SerializeField]
+    private Stat health;
+
+    /// <summary>
+    /// The player's mana
+    /// </summary>
+    [SerializeField]
+    private Stat mana;
+
+    /// <summary>
+    /// The player's initialHealth
+    /// </summary>
+    private float initHealth = 100;
+
+    /// <summary>
+    /// The player's initial mana
+    /// </summary>
+    private float initMana = 50;
+
+    protected override void Start()
+    {
+
+        health.Initialize(initHealth, initHealth);
+        mana.Initialize(initMana, initMana);
+
+        base.Start();
+    }
+
+    /// <summary>
+    /// We are overriding the characters update function, so that we can execute our own functions
+    /// </summary>
+    protected override void Update()
+    {
+        //Executes the GetInput function
+        GetInput();
+
+        base.Update();
+    }
+
+    /// <summary>
+    /// Listen's to the players input
+    /// </summary>
+    private void GetInput()
+    {
+        direction = Vector2.zero;
+
+        ///THIS IS USED FOR DEBUGGING ONLY
+        ///
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            health.MyCurrentValue -= 10;
+            mana.MyCurrentValue -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            health.MyCurrentValue += 10;
+            mana.MyCurrentValue += 10;
+        }
+
+
+        if (Input.GetKey(KeyCode.W)) //Moves up
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.A)) //Moves left
+        {
+            direction += Vector2.left; //Moves down
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D)) //Moves right
+        {
+            direction += Vector2.right;
+        }
+    }
+}
+
+
+/* using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is the player script, it contains functionality that is specific to the Player
+/// </summary>
+public class Player : Character
+{
+
+    [SerializeField]
+    private Stat health;
 
 	/// <summary>
     /// We are overriding the characters update function, so that we can execute our own functions
@@ -15,6 +110,7 @@ public class Player : Character
     {
         //Executes the GetInput function
         GetInput();
+        health.myCurrentValue = 100;
 
         base.Update();
 	}
@@ -43,4 +139,4 @@ public class Player : Character
             direction += Vector2.right;
         }
     }
-}
+} */
--- InscopeRPG/Assets/Scripts/Stat.cs
+++ InscopeRPG/Assets/Scripts/Stat.cs
@@ -5,6 +5,114 @@ using UnityEngine.UI;
 
 public class Stat : MonoBehaviour
 {
+
+    /// <summary>
+    /// The actual image that we are chaing the fill of
+    /// </summary>
+    private Image content;
+
+    /// <summary>
+    /// Hold the current fill value, we use this, so that we know if we need to lerp a value
+    /// </summary>
+    private float currentFill;
+
+    /// <summary>
+    /// The lerp speed
+    /// </summary>
+    [SerializeField]
+    private float lerpSpeed;
+
+    /// <summary>
+    /// The stat's maxValue for example max health or mana
+    /// </summary>
+    public float MyMaxValue { get; set; }
+
+    /// <summary>
+    /// The currentValue for example the current health or mana
+    /// </summary>
+    private float currentValue;
+
+    /// <summary>
+    /// Proprty for setting the current value, this has to be used every time we change the currentValue, so that everything updates correctly
+    /// </summary>
+    public float MyCurrentValue
+    {
+        get
+        {
+
+            return currentValue;
+        }
+
+        set
+        {
+            if (value > MyMaxValue)//Makes sure that we don't get too much health
+            {
+                currentValue = MyMaxValue;
+            }
+            else if (value < 0) //Makes sure that we don't get health below 0
+            {
+                currentValue = 0;
+            }
+            else //Makes sure that we set the current value withing the bounds of 0 to max health
+            {
+                currentValue = value;
+            }
+
+            //Calculates the currentFill, so that we can lerp
+            currentFill = currentValue / MyMaxValue;
+        }
+    }
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Creates a reference to the content
+        content = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Makes sure that we update the bar
+        HandleBar();
+    }
+
+    /// <summary>
+    /// Initialises the bar
+    /// </summary>
+    /// <param name="currentValue">The current value of the bar</param>
+    /// <param name="maxValue">The max value of the bar</param>
+    public void Initialize(float currentValue, float maxValue)
+    {
+        MyMaxValue = maxValue;
+        MyCurrentValue = currentValue;
+    }
+
+    /// <summary>
+    /// Makes sure that the bar updates
+    /// </summary>
+    private void HandleBar()
+    {
+        if (currentFill != content.fillAmount) //If we have a new fill amount then we know that we need to update the bar
+        {
+            //Lerps the fill amount so that we get a smooth movement
+            content.fillAmount = Mathf.Lerp(content.fillAmount, currentFill, Time.deltaTime * lerpSpeed);
+        }
+
+    }
+}
+
+
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stat : MonoBehaviour
+{
     private Image content; // Image content
 
     private float currentFill; // Current amount the fill is at for the image (for each game object this script is attached to)
@@ -36,6 +144,6 @@ public class Stat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(MyCurrentValue)
+        Debug.Log(MyCurrentValue);
     }
-}
+}*/
