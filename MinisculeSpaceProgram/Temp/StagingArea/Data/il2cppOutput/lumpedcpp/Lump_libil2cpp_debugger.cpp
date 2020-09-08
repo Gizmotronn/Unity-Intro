2d26528a5b3876c5d4d5962dd588c59aa020fe69
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-09-08 15:17:55 +0800

    #12 -- Prepping unity setup, assets & exploring / storyboarding
    Imported some assets for a trailer/splash screen
    https://www.notion.so/Tiny-Space-Program-4ac8ed36b4f3489ba6802b3ccf6056ae
    
    Clickup: see prev commits
    
    [@acord-robotics](https://github.com/acord-robotics) & @signal-kinetics

--- MinisculeSpaceProgram/Temp/StagingArea/Data/il2cppOutput/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Program Files\Unity\Hub\Editor\2019.3.13f1\Editor\Data\il2cpp\libil2cpp\debugger\il2cpp-stubs.cpp"
--- MinisculeSpaceProgram/Temp/StagingArea/Data/il2cppOutput/lumpedcpp/Lump_libil2cpp_mono-runtime.cpp
+++
@@ -1,3 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Program Files\Unity\Hub\Editor\2019.3.13f1\Editor\Data\il2cpp\libil2cpp\mono-runtime\il2cpp-callbacks.cpp"
-#include "C:\Program Files\Unity\Hub\Editor\2019.3.13f1\Editor\Data\il2cpp\libil2cpp\mono-runtime\il2cpp-mono-support.cpp"
