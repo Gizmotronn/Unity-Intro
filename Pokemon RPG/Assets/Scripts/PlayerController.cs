139d5d4f576a3480aa65cd92a40f8153997ddf1e
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-12-12 10:45:41 +0800

    Starting player creation and movement script
    https://github.com/acord-robotics/unity-intro/21
    @acord-robotics/game-development

---
+++ Pokemon RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public bool isMoving;
+    private Vector2 input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isMoving) {
+
+        }
+    }
+}
