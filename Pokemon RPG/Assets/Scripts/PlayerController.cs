ab66c1173806bfd63c286a40198f8ef9459e14eb
Liam Arbuckle <droidology@allianceofdroids.org.au>
2020-12-14 10:24:57 +0800

    Prev commit continue
    https://github.com/acord-robotics/Unity-Intro/issues/21

--- Pokemon RPG/Assets/Scripts/PlayerController.cs
+++ Pokemon RPG/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -19,15 +19,30 @@ public class NewBehaviourScript : MonoBehaviour
     void Update()
     {
         if (!isMoving) {
-            input.x = Input.GetAxisRaw("Horizontal");
+            input.x = Input.GetAxisRaw("Horizontal"); // 10:05 ep 1 - see notion for video link
             input.y = Input.GetAxisRaw("Vertical");
         }
 
         if (input != Vector2.zero)
         {
-            var targetPost = transform.position;
+            var targetPos = transform.position;
             targetPos.x += input.x;
-            targetPost.y += input.y;
+            targetPos.y += input.y;
+
+            StartCoroutine(Move(targetPos));
         }
+    }
+
+    IEnumerator Move(Vector3 targetPos) { // Do something over a period of time (IEnumerator) --> move the player over a period of time
+
+        isMoving = true;
+
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon) // Checks if the player is away from the target position - where we want to be
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null; // Stop the execution of the code when the player is at `targetPos`
+        }
+        transform.position = targetPos;
+        isMoving = false;
     }
 }
