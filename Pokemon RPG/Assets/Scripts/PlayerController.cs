3ffc8a33956d82f57f6d6a2e580be1c3418c7d5a
randomrok <68099802+randomrok@users.noreply.github.com>
2020-12-15 20:09:00 +0800

    Update PlayerController.cs
    This fixes the movement glitch/bug and improves on another part of the code (don't use while loops lol) 
    https://GitHub.com/acord-robotics/unity-intro/issues/22

--- Pokemon RPG/Assets/Scripts/PlayerController.cs
+++ Pokemon RPG/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,16 +18,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!isMoving) {
-            input.x = Input.GetAxisRaw("Horizontal"); // 10:05 ep 1 - see notion for video link
-            input.y = Input.GetAxisRaw("Vertical");
-
-            if (input.x != 0) input.y = 0; // Allowing the player to move diagonally.
-        }
+            input.x = Input.GetAxis("Horizontal"); // 10:05 ep 1 - see notion for video link
+            input.y = Input.GetAxis("Vertical");
 
         if (input != Vector2.zero)
         {
-            var targetPos = transform.position;
+            Vector3 targetPos = transform.position;
             targetPos.x += input.x;
             targetPos.y += input.y;
 
@@ -37,14 +33,15 @@ public class PlayerController : MonoBehaviour
 
     IEnumerator Move(Vector3 targetPos) { // Do something over a period of time (IEnumerator) --> move the player over a period of time
 
-        isMoving = true;
-
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon) // Checks if the player is away from the target position - where we want to be
+        if ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon) // Checks if the player is away from the target position - where we want to be
         {
+            isMoving = true;
             transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-            yield return null; // Stop the execution of the code when the player is at `targetPos`
+            yield return null; // Prevents execution of StartCoroutine(Move(_)) (see above), when the player is at `targetPos`
         }
-        transform.position = targetPos;
-        isMoving = false;
+        else isMoving = false;
+        //transform.position = targetPos;
+        //isMoving = false;
     }
+    
 }
