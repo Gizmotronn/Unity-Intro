b7a81274a6b7499e0ba88c239185296c3d68db67
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-01-02 00:21:17 +0800

    Creating "monsters" and fixing layerMask name
    In the previous commit we had the wrong name for Layer Mask; it did not correspond to the variable in the CheckForEncounters() function. We fixed this
    
    Issue: https://github.com/acord-robotics/unity-intro/issues/21
    
    Random encounters and long grass: https://www.notion.so/skinetics/Monster-Hunter-Applet-bcbdcd59d2a443c9bc278318b8ce241d#db4bba2b2baf4eb8abcfe3ca453568ad
    
    Creating monsters: https://www.notion.so/skinetics/Monster-Hunter-Applet-bcbdcd59d2a443c9bc278318b8ce241d#7bd089b6350d468f97a0365834e512e4
    
    Create an `enum` for Pokemon types. There is an error in Unity with this current commit but hopefully it will be fixed by the end of the video.

---
+++ Pokemon RPG/Assets/Scripts/Monsters/PokemonBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokemonBase : ScriptableObject
+{
+    [SerializeField] string name; // We need to use this variable outside of the class
+        
+    [TextField]
+    [SerializeField] string description;
+
+    // Variables for sprites
+    [SerializeField] Sprite frontSprite;
+    [SerializeField] Sprite backSprite; // We need both the front and back sprites of the monsters in the battle system (pocket monsters)
+}
+
+public enum PokemonType { // Will update this with our own list later
+    none,
+    Normal,
+    Fire,
+    Water,
+    Electric,
+    Grass,
+    Ice,
+    Fighting,
+    Poison,
+    Ground,
+    Flying,
+    Psychic,
+    Bug,
+    Rock,
+    Ghost,
+    Dragon
+}
--- Pokemon RPG/Assets/Scripts/PlayerController.cs
+++ Pokemon RPG/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public LayerMask solidObjectsLayer;
-    public LayerMask LongGrass;
+    public LayerMask grassLayer;
 
     private bool isMoving;
     private Vector2 input;
@@ -68,12 +68,14 @@ public class PlayerController : MonoBehaviour
         return true; // Call Move Coroutine if we return true
     }
 
-    private void CheckForEncounters()
+    private void CheckForEncounters() 
     {
-        if (Physics2D.OverlapCircle(transform.position, 0.2f, grassLayer) != null) { // If not null the player is walking in the long grass aka in contact with a gameObject with LongGrass layer
-            if (Random.Range(1,101) <= 10)
+        if (Physics2D.OverlapCircle(transform.position, 0.2f, grassLayer) != null) 
+        { // If not null the player is walking in the long grass aka in contact with a gameObject with LongGrass layer
+            if (Random.Range(1,101) <= 10) // 1 in 10 times when the player walks on grass he will trigger a battle with a "pokemon"
             {
-                Debug.Log("Encountered a wild alien!")
+                Debug.Log("Encountered a wild alien!");
             }
     }
+    } // https://stackoverflow.com/questions/61567592/first-time-using-c-it-says-i-have-error-cs1513#:~:text=Compiler%20Error%20CS1513means%20that%20there%20are%20not%20enough,the%20file.%20You%20should%20have%20three%20of%20them.
 }
