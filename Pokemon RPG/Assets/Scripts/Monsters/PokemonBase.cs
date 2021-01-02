7edef67be9f7ece5edf72a41f95bc03b1cf5576a
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-01-03 01:30:56 +0800

    Create Monster generation - Unity `CreateAssetMenu`
    Fix issue in prev commit (replace `TextField` with `TextArea` in `scripts/Monsters/PokemonBase.cs`
    
    https://github.com/acord-robotics/unity-intro/issues/21
    
    https://www.youtube.com/watch?v=x8B_eXfcj6U&t=178s
    
    https://www.notion.so/skinetics/Monster-Hunter-Applet-bcbdcd59d2a443c9bc278318b8ce241d#8e65808faccf43b78d39db0873b14524

--- Pokemon RPG/Assets/Scripts/Monsters/PokemonBase.cs
+++ Pokemon RPG/Assets/Scripts/Monsters/PokemonBase.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "Pokemon", menuName = "Pokemon/Create new pokemon")] // We can now create instances of Pokemon from the Unity inspector/project tab
+
 public class PokemonBase : ScriptableObject
 {
     [SerializeField] string name; // We need to use this variable outside of the class
         
-    [TextField]
+    [TextArea]
     [SerializeField] string description;
 
     // Variables for sprites
     [SerializeField] Sprite frontSprite;
     [SerializeField] Sprite backSprite; // We need both the front and back sprites of the monsters in the battle system (pocket monsters)
+
+    [SerializeField] PokemonType type1;
+    [SerializeField] PokemonType type2;
+
+    // Pokemon Stats
+    [SerializeField] int maxHp;
+    [SerializeField] int Attack;
+    [SerializeField] int Defense;
+    [SerializeField] int SpAttack;
+    [SerializeField] int SpDefense;
+    [SerializeField] int speed;
 }
 
 public enum PokemonType { // Will update this with our own list later
