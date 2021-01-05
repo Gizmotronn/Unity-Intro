5893261738ec8406fda74be2bc5a4d922860bf31
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-01-05 11:18:13 +0800

    Calculate monster attributes based on monster level - equaton
    https://github.com/acord-robotics/unity-intro/issues/21
    
    Prev commit Notion
    
    Need to think of possible other values/attributes for our monsters
    
    Might have an error relating to the class of "`Pokemon`" and how stats are being retrieved

--- Pokemon RPG/Assets/Scripts/Monsters/Pokemon.cs
+++ Pokemon RPG/Assets/Scripts/Monsters/Pokemon.cs
@@ -8,9 +8,30 @@ public class Pokemon
     int level; // Level of the Pokemon
 
     public Pokemon(PokemonBase pBase, int pLevel){
-        _base = pBase
-        _level = pLevel // OOP like in Python based on attributes of a class, in this case a class instance of a "Pokemon"
+        _base = pBase;
+        _level = pLevel; // OOP like in Python based on attributes of a class, in this case a class instance of a "Pokemon"
 
         _base.name;
+    }
+
+    // Calculate the value of the stats depending on the level
+    public int Attack {
+        get { return Math.FloorToInt((_base.Attack * level) / 100f) + 5; }  // Bulbapedia formula
+    }
+
+    public int Defense {
+        get { return Math.FloorToInt((_base.Defense * level) / 100f) + 5; } 
+    }
+
+    public int SpAttack {
+        get { return Math.FloorToInt((_base.SpAttack * level) / 100f) + 5; } 
+    }
+
+    public int SpDefense {
+        get { return Math.FloorToInt((_base.SpDefense * level) / 100f) + 5; } 
+    }
+
+    public int MaxHp {
+        get { return Math.FloorToInt((_base.MaxHp * level) / 100f) + 10; } 
     }
 }
