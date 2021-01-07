1a5d12912c1d3f5b5d311240de99f8e01b552fef
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-01-07 23:41:56 +0800

    Fixed error
    See Assets/Scripts/Monsters/Pokemon.cs
    
    ```cs
    using System.Collections;
    using System.Collections.Generic;
    using UnityEngine;
    using System;
    
    public class Pokemon {
        PokemonBase _base;
        int level;
    
        public Pokemon(PokemonBase pBase, int pLevel)
        {
            _base = pBase;
            level = pLevel;
    
            //_base.Name;
        }
    
        public int Attack {
            get { return Mathf.FloorToInt((_base.Attack * level) / 100f) +5; }
        }
        
        public int Defense {
            get { return Mathf.FloorToInt((_base.Defense * level) / 100f) +5; }
        }
    
        public int SpAttack {
            get { return Mathf.FloorToInt((_base.SpAttack * level) / 100f) +5; }
        }
    
        public int SpDefense {
            get { return Mathf.FloorToInt((_base.SpDefense * level) / 100f) +5; }
        }
    
        public int MaxHp {
            get { return Mathf.FloorToInt((_base.MaxHp * level) / 100f) +5; }
        }
    
        public int Speed {
            get { return Mathf.FloorToInt((_base.Speed * level) / 100f) +10; }
        }                    
    }
    ```
    
    Fix was `Mathf` rather than `Math`
    
    https://github.com/acord-robotics/unity-intro/issues/21

--- Pokemon RPG/Assets/Scripts/Monsters/Pokemon.cs
+++ Pokemon RPG/Assets/Scripts/Monsters/Pokemon.cs
@@ -16,26 +16,26 @@ public class Pokemon {
     }
 
     public int Attack {
-        get { return Math.FloorToInt((_base.Attack * level) / 100f) +5; }
+        get { return Mathf.FloorToInt((_base.Attack * level) / 100f) +5; }
     }
     
     public int Defense {
-        get { return Math.FloorToInt((_base.Defense * level) / 100f) +5; }
+        get { return Mathf.FloorToInt((_base.Defense * level) / 100f) +5; }
     }
 
     public int SpAttack {
-        get { return Math.FloorToInt((_base.SpAttack * level) / 100f) +5; }
+        get { return Mathf.FloorToInt((_base.SpAttack * level) / 100f) +5; }
     }
 
     public int SpDefense {
-        get { return Math.FloorToInt((_base.SpDefense * level) / 100f) +5; }
+        get { return Mathf.FloorToInt((_base.SpDefense * level) / 100f) +5; }
     }
 
     public int MaxHp {
-        get { return Math.FloorToInt((_base.MaxHp * level) / 100f) +5; }
+        get { return Mathf.FloorToInt((_base.MaxHp * level) / 100f) +5; }
     }
 
     public int Speed {
-        get { return Math.FloorToInt((_base.Speed * level) / 100f) +10; }
+        get { return Mathf.FloorToInt((_base.Speed * level) / 100f) +10; }
     }                    
 }
