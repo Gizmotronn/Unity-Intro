fb84f677102631673ccf3ce411c46e1e42d7117e
Liam Arbuckle <droidology@allianceofdroids.org.au>
2021-03-11 16:05:04 +0800

    UPK-15 Some minor changes, player control and collider, extending
    Comparing Pokemon RPG to our inventory and the addons to the inventory project, some being merged from Pokemon RPG.

--- Pokemon RPG/Assets/Scripts/Battle/BattleHud.cs
+++ Pokemon RPG/Assets/Scripts/Battle/BattleHud.cs
@@ -5,22 +5,21 @@ using UnityEngine.UI;
 
 public class BattleHud : MonoBehaviour
 {
+    Pokemon _pokemon;
+
     [SerializeField] Text nameText;
     [SerializeField] Text levelText;
     [SerializeField] HPBar hpBar;
 
-    Pokemon _pokemon;
-
     public void SetData(Pokemon pokemon) { // Links to Pokemon class in Pokemon.cs
         _pokemon = pokemon;
-
+        
         nameText.text = pokemon.Base.Name; 
         levelText.text = "Lvl " + pokemon.Level;
         hpBar.SetHP((float) pokemon.HP / pokemon.MaxHp);
     }
 
-    public IEnumerator UpdateHP()
-    {
-        yield return hpBar.SetHPSmooth((float)_pokemon.HP / _pokemon.MaxHp);
+    public IEnumerator UpdateHP() {
+        yield return hpBar.SetHPSmooth((float)_pokemon.HP / _pokemon.MaxHP);
     }
 }
